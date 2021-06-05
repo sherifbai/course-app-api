db6d3ddd49d66c7dc850e2c9ec5ae0645d311d2a
Sherifbai Ruzmetov <godness980@gmail.com>
2021-06-05 15:39:58 +0500

    configed multer

--- index.js
+++ index.js
@@ -1,15 +1,42 @@
 const express = require('express')
 const mongoose = require('mongoose')
+
+
 const bodyParser = require('body-parser')
+const multer = require('multer')
+
+
+const authRouter = require('./routes/auth')
+
 
 const app = express()
 
-const authRouter = require('./routes/auth')
 
 const url = "mongodb+srv://Sherif:rAzCmDang1ZCsYnc@cluster0.qwr9u.mongodb.net/course-app-api?retryWrites=true&w=majority"
 
 
+const fileStorage = multer.diskStorage({
+    destination: (req, file, cb) => {
+        cb(null, 'images')
+    },
+    filename: (req, file, cb) => {
+        cb(null, new Date().toISOString().replace(/:/g, '-') + '-' + file.originalname)
+    }
+})
+
+
+const fileFilter = (req, file, cb) => {
+    if (file.mimetype === 'image/png' || file.mimetype === 'image/jpg' || file.mimetype === 'image/jpeg') {
+        cb(null, true)
+    } else {
+        cb(null, false)
+    }
+}
+
+
 app.use(bodyParser.json())
+app.use(multer({storage: fileStorage, fileFilter: fileFilter}).single('image'))
+app.use('/images', express.static('images'))
 
 
 app.use((req, res, next) => {
