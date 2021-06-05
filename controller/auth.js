14dc664238a477e46f9b6a8f8497d5d8d31a97cc
Sherifbai Ruzmetov <godness980@gmail.com>
2021-06-05 15:23:19 +0500

    added validation to signup and login

--- controller/auth.js
+++ controller/auth.js
@@ -1,11 +1,19 @@
 const bcrypt = require('bcrypt')
 const jwt = require('jsonwebtoken')
+const { validationResult } = require('express-validator')
 
 const User = require('../models/user')
 
 exports.signUp = async (req, res, next) => {
     const email = req.body.email
     const password = req.body.password
+    const errors = validationResult(req)
+    
+    if (!errors.isEmpty()) {
+        const error = new Error('Validation failed')
+        error.statusCode = 422
+        throw error
+    }
 
     try {
         const hashedPW = await bcrypt.hash(password, 12)
@@ -35,6 +43,14 @@ exports.signUp = async (req, res, next) => {
 exports.signIn = async (req, res, next) => {
     const email = req.body.email
     const password = req.body.password
+    const errors = validationResult(req)
+    
+    if (!errors.isEmpty()) {
+        const error = new Error('Validation failed')
+        error.statusCode = 422
+        throw error
+    }
+
 
     try {
         const user = await User.findOne({email: email})
--- routes/auth.js
+++ routes/auth.js
@@ -1,10 +1,41 @@
 const { Router } = require('express')
+const { body } = require('express-validator')
 
+const User = require('../models/user')
 const authController = require('../controller/auth')
 
 const router = Router()
 
-router.post('/signup', authController.signUp)
-router.post('/signin', authController.signIn)
+router.post('/signup', [
+    body('email')
+        .isEmail()
+        .trim()
+        .not()
+        .isEmpty()
+        .custom((value, { req }) => {
+            return User.findOne({ email: value }).then(userDoc => {
+                if (!userDoc) {
+                    return Promise.reject('Email already used!!!')
+                }
+            })
+        }),
+    body('password')
+        .isLength({min: 5})
+        .trim()
+        .not()
+        .isEmpty()
+], authController.signUp)
+
+router.post('/signin', [
+    body('email')
+        .isEmail()
+        .not()
+        .isEmpty(),
+    body('password')
+        .isLength({min: 5})
+        .trim()
+        .not()
+        .isEmpty()
+], authController.signIn)
 
 module.exports = router
