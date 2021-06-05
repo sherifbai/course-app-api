6911e9417a7d555da5a1d9340acda0c606a5a75c
Sherifbai Ruzmetov <godness980@gmail.com>
2021-06-05 14:45:05 +0500

    added login func

--- controller/auth.js
+++ controller/auth.js
@@ -28,6 +28,51 @@ exports.signUp = async (req, res, next) => {
         if (!error.statusCode) {
             error.statusCode = 500
         }
-        res.status(error.statusCode).json(error);
+        next(error)
+    }
+}
+
+exports.signIn = async (req, res, next) => {
+    const email = req.body.email
+    const password = req.body.password
+
+    try {
+        const user = await User.findOne({email: email})
+
+        if (!user) {
+            const error = new Error('Email not founded!!!')
+            error.statusCode = 404
+            throw error
+        }
+
+        const isEqual = await bcrypt.compare(password, user.password)
+
+        if (!isEqual) {
+            const error = new Error('Wrong password!!!')
+            error.statusCode = 422
+            throw error
+        }
+
+        const token = jwt.sign(
+            {
+                email: email,
+                userId: user._id.toString()
+            },
+            "Sherif'sSecretKey",
+            { expiresIn: '1h' }
+        )
+
+        res.status(200).json({
+            success: true,
+            message: 'User logged in!',
+            token: token,
+            data: null
+        })
+
+    } catch (error) {
+        if (!error.statusCode) {
+            error.statusCode = 500
+        }
+        next(error)
     }
 }
--- routes/auth.js
+++ routes/auth.js
@@ -5,5 +5,6 @@ const authController = require('../controller/auth')
 const router = Router()
 
 router.post('/signup', authController.signUp)
+router.post('/signin', authController.signIn)
 
 module.exports = router
